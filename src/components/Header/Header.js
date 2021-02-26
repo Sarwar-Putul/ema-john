63b26543b39866a3fc45d5d69cf12d3d27095da1
Sarwar-Putul <sarwarputul@gmail.com>
2021-02-26 20:49:53 +0600

    Nav ber half done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <div className="header">
             <img src={logo} alt=""/>
+            <nav><a href="/Shop">Shop</a><a href="/Order Review">Order Review</a><a href="/Manage Inventory">Manage Inventory</a></nav>
         </div>
     );
 };
