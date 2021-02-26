371ad515cd4df4a6ebd8f30dac93cae657a7ddd9
Sarwar-Putul <sarwarputul@gmail.com>
2021-02-27 03:25:11 +0600

    button cart done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const product = (props) => {
-    console.log(props.product);
+    // console.log(props.product);
     const {name, img, seller, stock,price}= props.product;
     return (
         <div className="product">
@@ -15,6 +17,9 @@ const product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in the stock - order soon</small></p>
+                <button className="main-button"
+                onClick={()=> props.handleAddProduct(props.product)}
+                ><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
             </div>
             
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,15 +7,24 @@ import Product from '../Product/Product';
 const Shop = () => {
     const first10= fakeData.slice(0,10);
     const [products, setproducts] = useState(first10);
+    const [cart, setcart] = useState([])
+    const handleAddProduct = (product) =>{
+        console.log('click marche', product );
+    const newCart =[...cart, product];
+    setcart(newCart);
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
                 {
-                    products.map(pd => <Product product={pd}></Product>)
+                    products.map(pd => <Product 
+                        handleAddProduct = {handleAddProduct}
+                        product={pd}></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h1>This is a cart</h1>
+                <h5>Order Summary: {cart.length}</h5>
             </div>
            
         </div>
